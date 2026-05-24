ff854f1085d1bd5145ee78e472352ada083dcec8
qichenya <162135575+qichenya@users.noreply.github.com>
2026-05-24 19:47:49 +0800

    Update AnimatedRoutes.tsx

--- src/components/AnimatedRoutes.tsx
+++ src/components/AnimatedRoutes.tsx
@@ -11,7 +11,7 @@ const pageVariants = {
     y: 20,
     transition: {
       duration: 0.4,
-      ease: 'easeOut',
+      ease: 'easeOut' as const,
     },
   },
   in: {
@@ -19,7 +19,7 @@ const pageVariants = {
     y: 0,
     transition: {
       duration: 0.4,
-      ease: 'easeOut',
+      ease: 'easeOut' as const,
     },
   },
   out: {
@@ -27,7 +27,7 @@ const pageVariants = {
     y: -20,
     transition: {
       duration: 0.3,
-      ease: 'easeIn',
+      ease: 'easeIn' as const,
     },
   },
 };
