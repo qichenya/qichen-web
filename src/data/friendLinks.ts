d5f3850f8f21979beea4e7fb0b5dec246e999347
qichenya <qichengongzuoshi@gmail.com>
2026-07-24 14:28:05 +0800

    添加了更多友链并喝了一杯珍珠奶茶

--- src/data/friendLinks.ts
+++ src/data/friendLinks.ts
@@ -10,4 +10,13 @@ export const friendLinks: FriendLink[] = [
     url: 'https://4c01.cn',
     description: '一只笨猫的个人介绍',
   },
+  {
+    name: 'Cafe',
+    url: 'https://cafenya.top',
+    description: 'cafeawa的网站',
+  },  {
+    name: '罗伊',
+    url: 'https://www.roysgensokyo.space',
+    description: '欢迎来到我的小天地，在这里追寻我们的故事',
+  },
 ];
